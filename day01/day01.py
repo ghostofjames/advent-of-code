bc39f2e0adfc0c85dc6637762ab2ee4402a887b2
James Bloxham <11bloxhamj@gmail.com>
2022-11-12 16:34:44 +0000

    updated day01 to not use any imports

--- day01/day01.py
+++ day01/day01.py
@@ -1,20 +1,17 @@
-from itertools import pairwise
-
 data = []
 with open("day01_input.txt") as f:
     data = list(map(int, f.read().splitlines()))
 
-
 # Part 1
-count = 0
-for i, j in pairwise(data):
-    if j > i:
-        count += 1
-print(count)
+c = 0
+for i in range(0, len(data) - 1):
+    if sum(data[i+1:i+2]) > sum(data[i:i+1]):
+        c += 1
+print(f"Part 1: {c}")
 
 # Part 2
-count = 0
+c = 0
 for i in range(0, len(data) - 3):
-    if sum(data[i+1:i+4])> sum(data[i:i+3]) :
-        count += 1
-print(count)
+    if sum(data[i+1:i+4]) > sum(data[i:i+3]):
+        c += 1
+print(f"Part 2: {c}")
