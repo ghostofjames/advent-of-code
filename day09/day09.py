6002086952a4bd7716cd61bdbe80c3b6c01b6b85
James Bloxham <11bloxhamj@gmail.com>
2022-11-13 19:53:27 +0000

    day09 part 2

--- day09/day09.py
+++ day09/day09.py
@@ -2,25 +2,52 @@ import numpy as np
 from math import prod
 
 data = []
-# with open("day09_input.txt") as f:
-#     data = f.read().splitlines()
-with open("example.txt") as f:
+with open("day09_input.txt") as f:
     data = f.read().splitlines()
-heightmap = np.array([[int(x) for x in y] for y in data])
 
 
-def get_neighbours(y, x, heightmap):
+def get_neighbours(y, x, grid):
     neighbours = []
+
     if y > 0:
-        neighbours.append(heightmap[y - 1, x])
-    if y < heightmap.shape[0] - 1:
-        neighbours.append(heightmap[y + 1, x])
+        neighbours.append(grid[y - 1, x])
+    if y < grid.shape[0] - 1:
+        neighbours.append(grid[y + 1, x])
     if x > 0:
-        neighbours.append(heightmap[y, x - 1])
-    if x < heightmap.shape[1] - 1:
-        neighbours.append(heightmap[y, x + 1])
+        neighbours.append(grid[y, x - 1])
+    if x < grid.shape[1] - 1:
+        neighbours.append(grid[y, x + 1])
+
     return neighbours
 
+
+def count_basin_neighbours(y, x, grid, visited):
+    visited[y, x] = 1
+    count = 0
+
+    if y > 0:
+        if grid[y - 1, x] < 9 and not visited[y - 1, x]:
+            count += 1 + count_basin_neighbours(y - 1, x, grid, visited)
+    if y < grid.shape[0] - 1:
+        if grid[y + 1, x] < 9 and not visited[y + 1, x]:
+            count += 1 + count_basin_neighbours(y + 1, x, grid, visited)
+    if x > 0:
+        if grid[y, x - 1] < 9 and not visited[y, x - 1]:
+            count += 1 + count_basin_neighbours(y, x - 1, grid, visited)
+    if x < grid.shape[1] - 1:
+        if grid[y, x + 1] < 9 and not visited[y, x + 1]:
+            count += 1 + count_basin_neighbours(y, x + 1, grid, visited)
+
+    return count
+
+
+def get_basin_size(y, x, heightmap):
+    visited = np.zeros(heightmap.shape)
+    size = 1 + count_basin_neighbours(y, x, heightmap, visited)
+    return size
+
+
+heightmap = np.array([[int(x) for x in y] for y in data])
 
 # Part 1
 risk_level = 0
@@ -32,22 +59,13 @@ for iy, ix in np.ndindex(heightmap.shape):
 
 print(f"Part 1: {risk_level}")
 
-# # Part 2
-# basin_sizes = []
-# for iy, ix in np.ndindex(heightmap.shape):
-#     height = heightmap[iy, ix]
-#     neighbours = []
-#     if iy > 0:
-#         neighbours.append(heightmap[iy - 1, ix])
-#     if iy < heightmap.shape[0] - 1:
-#         neighbours.append(heightmap[iy + 1, ix])
-#     if ix > 0:
-#         neighbours.append(heightmap[iy, ix - 1])
-#     if ix < heightmap.shape[1] - 1:
-#         neighbours.append(heightmap[iy, ix + 1])
 
-#     if all(height < n for n in get_neighbours(iy, ix, heightmap)):
-#         # get basin size
-#         pass
+# Part 2
+basin_sizes = []
+for iy, ix in np.ndindex(heightmap.shape):
+    height = heightmap[iy, ix]
 
-# print(f"Part 2: {prod(basin_sizes[-3:])}")
+    if all(height < n for n in get_neighbours(iy, ix, heightmap)):
+        basin_sizes.append(get_basin_size(iy, ix, heightmap))
+
+print(f"Part 2: {prod(sorted(basin_sizes)[-3:])}")
