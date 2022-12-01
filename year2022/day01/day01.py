6b3ff2063a727bbe9565b74c0b8c59f8e2bada7a
James Bloxham <11bloxhamj@gmail.com>
2022-12-01 09:50:05 +0000

    2020 01

---
+++ year2022/day01/day01.py
@@ -0,0 +1,13 @@
+with open("input.txt") as f:
+    data = f.read()
+
+
+# Part 1
+elfs = [[int(item) for item in elf.splitlines()] for elf in data.split('\n\n')]
+calories = [sum(items for items in elf) for elf in elfs]
+
+print(f"Part 1: {max(calories)}")
+
+
+# Part 2
+print(f"Part 2: {sum(sorted(calories)[-3:])}")
